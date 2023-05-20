6556bb4d1bc29fa2b17a0030803cbfbbae59c57f
realYerkezhan <124233409+realYerkezhan@users.noreply.github.com>
2023-05-20 18:20:27 +0600

    Add files via upload

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
-import(
-	"net/http"
-	"github.com/gin-gonic/gin"
-	"strconv"
+import (
 	"dijkstraproject/dijkstra" // Full import path relative to your module
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
+
 type path struct {
-	StartPoint int `json:"from"`
-	EndPoint int `json:"to"`
-	Path []string `json:"path"`
-	ShortestDistance float32 `json:"distance"`
+	StartPoint       int      `json:"from"`
+	EndPoint         int      `json:"to"`
+	Path             []string `json:"path"`
+	ShortestDistance float32  `json:"distance"`
+	PathIndex        []int    `json:"index"`
 }
 
 func getPaths(c *gin.Context) {
@@ -30,34 +33,33 @@ func getPaths(c *gin.Context) {
 		return
 	}
 
-
 	// // Run Dijkstra's algorithm
 	// distances, previous := dijkstra.DijkstraParallel(start, end)
 
 	// // Get the shortest path
 	// shortestPath := dijkstra.GetPath(previous, end)
 
-	shortestPath, shortestDistance := dijkstra.Initialize(start,end)
+	shortestPath, shortestDistance, myindex := dijkstra.Initialize(start, end)
 
 	response := struct {
 		StartPoint       int      `json:"start"`
 		EndPoint         int      `json:"end"`
 		Path             []string `json:"path"`
 		ShortestDistance float64  `json:"distance"`
+		PathIndex        []int    `json:"index"`
 	}{
 		StartPoint:       start,
 		EndPoint:         end,
 		Path:             shortestPath,
 		ShortestDistance: shortestDistance,
+		PathIndex:        myindex,
 	}
-	
-	 c.IndentedJSON(http.StatusOK, response)
+
+	c.IndentedJSON(http.StatusOK, response)
 }
 
-
-func main() { 
+func main() {
 	router := gin.Default()
 	router.GET("/shortestpath", getPaths)
 	router.Run("localhost:9090")
 }
-
